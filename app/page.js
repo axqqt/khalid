6259a490392f35b9b1520c16ef7b6137d8af72a8
Veloxal <127298642+axqqt@users.noreply.github.com>
2024-11-09 23:36:51 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client"
+import Contact from "@/componets/home/Contact";
+import Hero from "@/componets/home/Hero";
+import Services from "@/componets/home/Services";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Contact />
+    </>
+  );
+}
